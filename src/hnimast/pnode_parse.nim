e17582a4285fa483bf14996dc4b92665303acf55
haxscramper <haxscramper@gmail.com>
2021-02-10 00:13:57 +0300

    [FIX] Don't `quit` on parse errors

--- src/hnimast/pnode_parse.nim
+++ src/hnimast/pnode_parse.nim
@@ -1,10 +1,13 @@
 import compiler/[parser, llstream, idents, options, pathutils, astalgo]
-import compiler/[ast]
+import compiler/[ast, lineinfos]
 
 proc parsePNodeStr*(str: string): PNode =
   let cache: IdentCache = newIdentCache()
   let config: ConfigRef = newConfigRef()
   var pars: Parser
+
+  config.verbosity = 0
+  config.options.excl optHints
 
   openParser(
     p = pars,
@@ -13,6 +16,11 @@ proc parsePNodeStr*(str: string): PNode =
     cache = cache,
     config = config
   )
+
+  pars.lex.errorHandler =
+    proc(conf: ConfigRef; info: TLineInfo; msg: TMsgKind; arg: string) =
+      if msg notin {hintLineTooLong}:
+        echo arg
 
   result = parseAll(pars)
   closeParser(pars)
