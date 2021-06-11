f9b324a53bb277ee1652ff52fad302e77ec04b79
haxscramper <haxscramper@gmail.com>
2021-06-11 22:07:55 +0300

    [TEST]

--- tests/tCompilerAux.nim
+++ tests/tCompilerAux.nim
@@ -1,1 +1,10 @@
-import hnimast/compiler_aux
+import hnimast/compiler_aux, std/os
+# import hpprint
+
+import std/[tables, sets]
+
+let p = currentSourcePath.parentDir.parentDir / "hnimast.nimble"
+let c = readFile(p)
+let info = parsePackageInfoNims(c)
+
+# pprint info
